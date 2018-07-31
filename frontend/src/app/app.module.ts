05d01af08498e3fdadbfb380c60354623e8b4abc
shrawan <shrawan@accessibleict.com>
2018-07-31 18:29:14 +0545

    app module updated

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+ 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
